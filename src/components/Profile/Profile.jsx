fa80f30b0917ff0084da2431bf8e52e698320ac7
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-07-13 16:51:05 +0300

    update

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,51 @@
+import PropTypes from 'prop-types';
+import defaultImage from '../../utils/defaultImage.jpg';
+import { ProfileCard } from './Profile.styled'
+
+const Profile = (props) => {
+    const { username, tag, location, avatar, stats } = props;
+
+  console.log(props);
+
+    return <ProfileCard className="profile">
+        <div className="description">
+            <img
+                src={avatar ?? defaultImage}
+                alt={tag}
+                className="avatar"
+            />
+            <p className="name">{ username }</p>
+            <p className="tag">@{ tag }</p>
+            <p className="location">{location}</p>
+        </div>
+    
+        <ul className="stats">
+            <li>
+                <span className="label">Followers</span>
+                <span className="quantity">{ stats.followers }</span>
+            </li>
+            <li>
+                <span className="label">Views</span>
+                <span className="quantity">{ stats.views }</span>
+            </li>
+            <li>
+                <span className="label">Likes</span>
+                <span className="quantity">{ stats.likes }</span>
+            </li>
+        </ul>
+    </ProfileCard>
+};
+
+Profile.propTypes = {
+    avatar: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    stats: PropTypes.shape({
+        followers: PropTypes.number.isRequired,
+        views: PropTypes.number.isRequired,
+        likes: PropTypes.number.isRequired,      
+    }),
+};
+
+export default Profile;
---
+++ src/components/Profile/Profile.styled.js
@@ -0,0 +1,6 @@
+import { styled } from 'styled-components'
+
+export const ProfileCard = styled.div`
+    border: 2px solid red;
+    margin: auto;
+`
